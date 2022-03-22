b0b890eb2e8d50901fbf1b771df31145078ab21c
Eunji Kim <70746467+eunji8784@users.noreply.github.com>
2022-03-22 18:30:49 +0900

    p.99 1이 될 때까지

--- Algorithm/ch3.그리디/until1.py
+++ Algorithm/ch3.그리디/until1.py
@@ -1,13 +1,11 @@
 n, k = map(int, input().split())
 
 count = 0
-quota = 0
 
 while n != 1:
   if n % k == 0:
-    quota = n // k
+    n //= k
     count += 1
-    n = quota
   else:
     n -= 1
     count += 1
