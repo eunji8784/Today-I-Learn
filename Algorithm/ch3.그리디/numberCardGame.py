0fe5717d820c4e451320985039a7bd225a4eac08
Eunji Kim <70746467+eunji8784@users.noreply.github.com>
2022-03-22 17:29:09 +0900

    p.96 숫자 카드 게임

--- Algorithm/ch3.그리디/numberCardGame.py
+++ Algorithm/ch3.그리디/numberCardGame.py
@@ -2,10 +2,9 @@ n, m = map(int, input().split())
 
 result = 0
 
-# 행의 수(n) 만큼 입력받기
 for i in range(n):
   data = list(map(int, input().split()))
-  min_value = min(data) #입력 받은 수에서 가장 작은 값 찾기
+  min_value = min(data)
   result = max(result, min_value)
 
 print(result)
