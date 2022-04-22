6d78f76e246ac935e9984c2d75f92b960a397f0f
Eunji Kim <70746467+eunji8784@users.noreply.github.com>
2022-04-22 20:30:36 +0900

    p.180 성적이 낮은 순서로 학생 출력하기

--- Algorithm/ch6.정렬/student.py
+++ Algorithm/ch6.정렬/student.py
@@ -3,5 +3,8 @@ import sys
 n = int(sys.stdin.readline())
 list = [[*sys.stdin.readline().split()] for _ in range(n)]
 
+for j in range(len(list)):
+  list[j][1] = int(list[j][1])
+
 for i in sorted(list, key=lambda x:x[1]):
   print(i[0], end = ' ')
